7b303326ef8dad79d84a704cd91327c6a6a74667
Artur Neumann <artur@jankaritech.com>
2018-08-06 15:18:39 +0545

    create user with default password

--- tests/acceptance/features/bootstrap/Provisioning.php
+++ tests/acceptance/features/bootstrap/Provisioning.php
@@ -264,9 +264,16 @@ trait Provisioning {
 			} else {
 				$email = null;
 			}
+
+			if (isset($row['password'])) {
+				$password = $row['password'];
+			} else {
+				$password = $this->getPasswordForUser($row ['username']);
+			}
+
 			$this->createUser(
 				$row ['username'],
-				$row ['password'],
+				$password,
 				$displayName,
 				$email,
 				($doNotInitialize === "")
